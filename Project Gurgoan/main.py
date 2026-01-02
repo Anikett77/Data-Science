4122e26ec6553e49476d494785d622a7344e334b
Anikett77 <aniketchaturvedi83@gmail.com>
2026-01-02 21:20:36 +0530

    Preparing Project Gurgoan

---
+++ Project Gurgoan/main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+housing = pd.read_csv("housing.csv")
+
+housing["income_cat"] = pd.cut(
+    housing["median_income"],
+    bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+ labels=[1,2,3,4,5]
+)
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index].drop("income_cat", axis=1)
+    strat_test_set = housing.loc[test_index].drop("income_cat", axis=1)
+
+
+housing = strat_train_set.copy()
+
+housing_labels = housing["median_house_value"].copy()
+housing = housing.drop("median_house_value", axis=1)
+
+print(housing, housing_labels)
