3bbc721b5b3c083152ceb59aff0e24514873f445
Anikett77 <aniketchaturvedi83@gmail.com>
2026-01-06 18:32:54 +0530

    cross vaildation on decision tree

--- Project Gurgoan/main.py
+++ Project Gurgoan/main.py
@@ -5,6 +5,13 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn.model_selection import cross_val_score
+
 
 housing = pd.read_csv("housing.csv")
 
@@ -47,3 +54,43 @@ full_pipeline = ColumnTransformer([
 
 housing_prepared = full_pipeline.fit_transform(housing)
 print(housing_prepared.shape)
+
+
+# Linear Regression
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+
+# Decision Tree
+tree_reg = DecisionTreeRegressor(random_state=42)
+tree_reg.fit(housing_prepared, housing_labels)
+
+# Random Forest
+forest_reg = RandomForestRegressor(random_state=42)
+forest_reg.fit(housing_prepared, housing_labels)
+
+# PREDICTIONS
+lin_preds = lin_reg.predict(housing_prepared)
+tree_preds = tree_reg.predict(housing_prepared)
+forest_preds = forest_reg.predict(housing_prepared)
+
+# RMSE (VERSION-SAFE)
+
+lin_rmse = sqrt(mean_squared_error(housing_labels, lin_preds))
+tree_rmse = sqrt(mean_squared_error(housing_labels, tree_preds))
+forest_rmse = sqrt(mean_squared_error(housing_labels, forest_preds))
+
+# OUTPUT
+print("Linear Regression RMSE :", lin_rmse)
+print("Decision Tree RMSE     :", tree_rmse)
+print("Random Forest RMSE     :", forest_rmse)
+
+tree_rmses =-cross_val_score(
+    tree_reg,
+    housing_prepared,
+    housing_labels,
+    scoring="neg_root_mean_squared_error",
+    cv=10
+)
+print("Decision Tree CV RMSEs:", tree_rmses)
+print("\nCross-Validation Performance (Decision Tree):")
+print(pd.Series(tree_rmses).describe())
